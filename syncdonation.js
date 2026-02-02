9f2921c07eda3aa696b6e017d2eba869d84a4850
Harshit Rai <harshitrai004@gmail.com>
2026-02-02 18:48:27 +0530

    Sync donation issue fixed

--- syncdonation.js
+++ syncdonation.js
@@ -1,7 +1,7 @@
-import admin from "firebase-admin";
-import fs from "fs";
+const admin = require("firebase-admin");
+const fs = require("fs");
 const serviceAccount = JSON.parse(
-    fs.readFileSync("./serviceAccountKey.json", "utf8")
+    fs.readFileSync("./serviceAccountkey.json", "utf8")
 );
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
